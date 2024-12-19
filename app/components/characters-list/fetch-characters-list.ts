0932cfd80a3cf387707146c51a9a8b457fccf136
Guillermo Cifre <guillecg13@gmail.com>
2024-12-19 15:47:54 +0100

    Deleted old files

--- app/components/characters-list/fetch-characters-list.ts
+++
@@ -1,5 +0,0 @@
-export async function fetchCharactersList() {
-  const res = await fetch("https://rickandmortyapi.com/api/character");
-  if (!res.ok) throw new Error("Error to load all characters");
-  return await res.json();
-}
--- app/components/characters-shared-episodes/fetch-episodes.ts
+++
@@ -1,5 +0,0 @@
-export async function fetchEpisodes() {
-  const res = await fetch("https://rickandmortyapi.com/api/episode");
-  if (!res.ok) throw new Error("Error to load all episodes");
-  return await res.json();
-}
